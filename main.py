b89d4533b77280f78166f9189daf76f840871712
adam <adamvonbaron@outlook.com>
2021-01-02 17:01:08 -0500

    fix the thing

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ template_lookup = TemplateLookup(directories=['templates'])
 
 @app.route("/starred_repos")
 def get_starred_repos():
-    get_starred_repos_template = Template(filename="templates/get_user.mako", lookup=template_lookup)
+    get_starred_repos_template = template_lookup.get_template("get_user.mako")
     return get_starred_repos_template.render()
 
 @app.route("/starred_repos/<username>")
@@ -22,5 +22,5 @@ def show_starred_repos(username):
         }
         for repo in json_resp
     ]
-    starred_repos_template = Template(filename="templates/starred_repos.mako", lookup=template_lookup)
+    starred_repos_template = template_lookup.get_template("starred_repos.mako")
     return starred_repos_template.render(repos=repo_names, user=username)
